a5db829b800114213cd4b0a4fe8c3f5223a8c9e2
imanwarsame <iwarsame38@gmail.com>
2023-07-08 09:38:45 +0100

    Timeline added

--- client/src/components/experience/Work.tsx
+++ client/src/components/experience/Work.tsx
@@ -1,4 +1,6 @@
-import { Box, Paper, Typography } from '@mui/material';
+import { Timeline, TimelineItem, TimelineSeparator, TimelineDot, TimelineConnector, TimelineContent, TimelineOppositeContent } from '@mui/lab';
+import { Box, Typography } from '@mui/material';
+import WorkOutlineOutlinedIcon from '@mui/icons-material/WorkOutlineOutlined';
 
 export default function Work() {
 
@@ -9,9 +11,83 @@ export default function Work() {
 			alignItems: 'center',
 			justifyContent: 'center'
 		}}>
-			<Paper sx={{ width: '60vw', minHeight: '300px', padding: 3, backgroundColor: 'blueviolet' }}>
-				<Typography>Hello</Typography>
-			</Paper>
+			<Timeline position="alternate">
+				<TimelineItem>
+					<TimelineOppositeContent
+						sx={{ m: 'auto 0' }}
+						align="right"
+						variant="body2"
+						color="text.secondary"
+					>
+          9:30 am
+					</TimelineOppositeContent>
+					<TimelineSeparator>
+						<TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+						<TimelineDot color="primary" variant="outlined">
+							<WorkOutlineOutlinedIcon />
+						</TimelineDot>
+						<TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+					</TimelineSeparator>
+					<TimelineContent sx={{ py: '12px', px: 2 }}>
+						<Typography variant="h6" component="span">
+            Eat
+						</Typography>
+						<Typography>Because you need strength</Typography>
+					</TimelineContent>
+				</TimelineItem>
+				<TimelineItem>
+					<TimelineOppositeContent
+						sx={{ m: 'auto 0' }}
+						variant="body2"
+						color="text.secondary"
+					>
+          10:00 am
+					</TimelineOppositeContent>
+					<TimelineSeparator>
+						<TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+						<TimelineDot color="primary" variant="outlined">
+							<WorkOutlineOutlinedIcon />
+						</TimelineDot>
+						<TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+					</TimelineSeparator>
+					<TimelineContent sx={{ py: '12px', px: 2 }}>
+						<Typography variant="h6" component="span">
+            Code
+						</Typography>
+						<Typography>Because it&apos;s awesome!</Typography>
+					</TimelineContent>
+				</TimelineItem>
+				<TimelineItem>
+					<TimelineSeparator>
+						<TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+						<TimelineDot color="primary" variant="outlined">
+							<WorkOutlineOutlinedIcon />
+						</TimelineDot>
+						<TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+					</TimelineSeparator>
+					<TimelineContent sx={{ py: '12px', px: 2 }}>
+						<Typography variant="h6" component="span">
+            Sleep
+						</Typography>
+						<Typography>Because you need rest</Typography>
+					</TimelineContent>
+				</TimelineItem>
+				<TimelineItem>
+					<TimelineSeparator>
+						<TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+						<TimelineDot color="primary" variant="outlined">
+							<WorkOutlineOutlinedIcon />
+						</TimelineDot>
+						<TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+					</TimelineSeparator>
+					<TimelineContent sx={{ py: '12px', px: 2 }}>
+						<Typography variant="h6" component="span">
+							Repeat
+						</Typography>
+						<Typography>Because this is the life you love!</Typography>
+					</TimelineContent>
+				</TimelineItem>
+			</Timeline>
 		</Box>
 	);
 }
