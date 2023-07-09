2622b0befba77626c9c3a548c34a807cd66fe977
imanwarsame <iwarsame38@gmail.com>
2023-07-09 18:14:11 +0100

    Contact page done

--- client/src/components/contact/Contact.tsx
+++ client/src/components/contact/Contact.tsx
@@ -1,8 +1,63 @@
-import { Typography } from '@mui/material';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { TextField, Button, Box, Grid, Typography } from '@mui/material';
 
 export default function Contact() {
+	const form = useRef<HTMLFormElement>(null);
 
-	return(
-		<Typography>Contact</Typography>
+	const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		if (form.current) {
+			emailjs.sendForm('service_9ajbpvi', 'template_z1ct7br', form.current, 'XzAdvhqwE_a6T_Sn5')
+				.then(
+					(result) => {
+						console.log(result.text);
+						form.current?.reset();
+					},
+					(error) => {
+						console.log(error.text);
+					}
+				);
+		}
+	};
+
+	return (
+		<Box sx={{
+			height: '100vh',
+			display: 'flex',
+			flexDirection: 'column',
+			alignItems: 'center',
+			justifyContent: 'center',
+			backgroundColor: 'secondary.main'
+		}}>
+			<Typography variant='h4'>Get in touch!</Typography>
+			<form ref={form} onSubmit={sendEmail} className='contact_form'>
+				<Grid container spacing={2} sx={{ padding: 10 }}>
+					<Grid item xs={12} md={6}>
+						<TextField required fullWidth type='text' name='user_name' id="outlined-required" label="Name"/>
+					</Grid>
+					<Grid item xs={12} md={6}>
+						<TextField required fullWidth type='email' name='user_email' id="outlined-required" label="Email"/>
+					</Grid>
+					<Grid item xs={12}>
+						<TextField
+							required
+							fullWidth
+							type='text'
+							name='message'
+							id="outlined-multiline-flexible"
+							label="Message"
+							multiline
+							rows={4}
+						/>
+					</Grid>
+					<Grid item xs={12}>
+						<Button type='submit' variant='outlined'>Submit</Button>
+					</Grid>
+				</Grid>
+			</form>
+		</Box>
+
 	);
 }
