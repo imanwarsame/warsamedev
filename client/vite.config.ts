816ae52b9d5ec4f0c20eb5704f9c78eaf0d32046
imanwarsame <iwarsame38@gmail.com>
2024-01-26 18:16:31 +0000

    Added local network and blob file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -17,5 +17,10 @@ export default defineConfig({
 				resources: 'usable',
 			},
 		},
-	}
+	},
+	server:
+    {
+        host: true, // Open to local network and display URL
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+    },
 });
