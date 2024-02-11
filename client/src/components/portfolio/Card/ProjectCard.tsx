751025080bf4990b2eeef0f686d01b08378b2905
imanwarsame <iwarsame38@gmail.com>
2024-02-11 12:30:47 +0000

    Fixed errors with build

--- client/src/components/portfolio/Card/ProjectCard.tsx
+++ client/src/components/portfolio/Card/ProjectCard.tsx
@@ -2,18 +2,21 @@ import { Box } from '@mui/material';
 import { motion } from 'framer-motion';
 import { PortfolioItem } from './Types';
 
-export default function ProjectCard ({ data, open }: { data: PortfolioItem, open: () => void }) {
-
+export default function ProjectCard({ data, open }: { data: PortfolioItem; open: () => void }) {
 	return (
 		<motion.div className='project_card' onClick={open} whileHover={{ scale: 1.1 }}>
-			<Box sx={{
-				backgroundImage: `url(${data.imageUrl})`,
-				borderRadius: 5,
-				border: 1,
-				backgroundRepeat: 'no-repeat',
-				backgroundPosition: 'center center',
-				backgroundSize: 'cover', height: '300px' }}>
-			</Box>
+			<Box
+				component='div'
+				sx={{
+					backgroundImage: `url(${data.imageUrl})`,
+					borderRadius: 5,
+					border: 1,
+					backgroundRepeat: 'no-repeat',
+					backgroundPosition: 'center center',
+					backgroundSize: 'cover',
+					height: '300px',
+				}}
+			></Box>
 		</motion.div>
 	);
 }
--- client/src/components/portfolio/Card/ProjectModal.tsx
+++ client/src/components/portfolio/Card/ProjectModal.tsx
@@ -5,27 +5,27 @@ import { PortfolioItem } from './Types';
 import GithubIcon from './github.svg';
 import WebIcon from './earth.svg';
 
-
-export default function ProjectModal ({ data, close }: { data: PortfolioItem, close: () => void }) {
-
+export default function ProjectModal({ data, close }: { data: PortfolioItem; close: () => void }) {
 	const container = {
 		hidden: { opacity: 0 },
 		show: {
 			opacity: 1,
 			transition: {
-				staggerChildren: 0.5
-			}
-		}
+				staggerChildren: 0.5,
+			},
+		},
 	};
 
 	const item = {
 		hidden: { opacity: 0, y: '10vh' },
-		show: { opacity: 1, y: '0vh' }
+		show: { opacity: 1, y: '0vh' },
 	};
 
 	return (
-		<motion.div variants={container} initial="hidden" animate="show">
-			<Box position='fixed'
+		<motion.div variants={container} initial='hidden' animate='show'>
+			<Box
+				component='div'
+				position='fixed'
 				sx={{
 					backgroundColor: 'rgba(255,255,255,0.75)',
 					backdropFilter: 'blur(20px)',
@@ -33,31 +33,44 @@ export default function ProjectModal ({ data, close }: { data: PortfolioItem, cl
 					width: { xs: '80vw', md: '50vw' },
 					left: '50%',
 					transform: 'translateX(-50%)',
-					top: '50px'
+					top: '50px',
 				}}
 			>
 				<img src={data.imageUrl} width='100%'></img>
-				<Box sx={{ display: 'flex', alignContent: 'center', justifyContent: 'center' }}>
+				<Box
+					component='div'
+					sx={{ display: 'flex', alignContent: 'center', justifyContent: 'center' }}
+				>
 					<motion.div>
 						<motion.div variants={item}>
 							<Typography>{data.description}</Typography>
 						</motion.div>
 						<Stack direction='row' justifyContent='center'>
 							<motion.div variants={item}>
-								<IconButton size='medium' href={data.githubLink} target="_blank" rel="noopener noreferrer">
-									<img src={GithubIcon} alt='Silhoutte of an octopus cat hybrid' height={30}/>
+								<IconButton
+									size='medium'
+									href={data.githubLink}
+									target='_blank'
+									rel='noopener noreferrer'
+								>
+									<img src={GithubIcon} alt='Silhoutte of an octopus cat hybrid' height={30} />
 								</IconButton>
 							</motion.div>
 							<motion.div variants={item}>
-								<IconButton size='medium' href={data.webLink} target="_blank" rel="noopener noreferrer">
-									<img src={WebIcon} alt='Planet earth, signifying website' height={30}/>
+								<IconButton
+									size='medium'
+									href={data.webLink}
+									target='_blank'
+									rel='noopener noreferrer'
+								>
+									<img src={WebIcon} alt='Planet earth, signifying website' height={30} />
 								</IconButton>
 							</motion.div>
 						</Stack>
 					</motion.div>
 				</Box>
 				<IconButton size='medium' onClick={close} sx={{ alignSelf: 'center' }}>
-					<CancelOutlinedIcon/>
+					<CancelOutlinedIcon />
 				</IconButton>
 			</Box>
 		</motion.div>
