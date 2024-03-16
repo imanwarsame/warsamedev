7e43b800f95f1a4d3d953f167d99f79747a1626d
imanwarsame <iwarsame38@gmail.com>
2024-03-16 17:32:39 +0000

    Fixed dark mode persistence

--- client/src/store.ts
+++ client/src/store.ts
@@ -5,10 +5,20 @@ interface CounterState {
 	setDarkMode: (mode: boolean) => void;
 }
 
-export const useDevStore = create<CounterState>((set) => ({
-	darkMode: localStorage.getItem('darkMode') === 'true' || false, // Set default value to the value in local storage or false if not found
-	setDarkMode: (mode: boolean) => {
-		set(() => ({ darkMode: mode }));
-		localStorage.setItem('IW_dark_mode', mode.toString()); // Save dark mode to local storage
-	},
-}));
+export const useDevStore = create<CounterState>((set) => {
+	//Retrieve dark mode value from local storage
+	const savedDarkMode = localStorage.getItem('IW_dark_mode');
+
+	//Determine default value based on local storage or default to false
+	const defaultDarkMode = savedDarkMode ? savedDarkMode === 'true' : false;
+
+	return {
+		darkMode: defaultDarkMode,
+		setDarkMode: (mode: boolean) => {
+			// Save dark mode to local storage
+			localStorage.setItem('IW_dark_mode', mode.toString());
+			// Update dark mode state
+			set(() => ({ darkMode: mode }));
+		},
+	};
+});
