0345cd387dd35238a215756ce1248bcf2630de98
imanwarsame <iwarsame38@gmail.com>
2024-03-17 18:12:41 +0000

    Updated test

--- client/src/components/__tests__/Home.test.tsx
+++ client/src/components/__tests__/Home.test.tsx
@@ -1,31 +1,27 @@
-import { describe, expect, test, vi } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import userEvent  from '@testing-library/user-event';
 import Home from '../home/Home';
 
 describe('Home component test', () => {
 	test('should render text', () => {
-
-		render(<Home/>);
-
-		expect(screen.getByText(/Full Stack Developer | Civil Engineer | Architectural Photographer/i)).toBeDefined();
-	});
-	test('social buttons are clicked', async () => {
 		render(<Home />);
-		const user = userEvent.setup();
 
-		const spyAnchorTag = vi.spyOn(user, 'click');
-
-		//Github link
-		await user.click(screen.getByAltText('Silhoutte of an octopus cat hybrid'));
-		expect(spyAnchorTag).toHaveBeenCalled();
-
-		//Twitter link
-		await user.click(screen.getByAltText('Twitter logo of tweeting bird'));
-		expect(spyAnchorTag).toHaveBeenCalled();
-
-		//LinkedIn link
-		await user.click(screen.getByAltText('The letters i and n representing the LinkedIn logo'));
-		expect(spyAnchorTag).toHaveBeenCalled();
+		expect(screen.getByText('Iman Warsame')).toBeDefined();
 	});
+
+	// test('social buttons are clicked', async () => {
+	// 	render(<Home />);
+	// 	global.innerWidth = 5000;
+	// 	const user = userEvent.setup();
+
+	// 	const spyAnchorTag = vi.spyOn(user, 'click');
+
+	// 	//Github link
+	// 	await user.click(screen.getByAltText('Silhoutte of an octopus cat hybrid'));
+	// 	expect(spyAnchorTag).toHaveBeenCalled();
+
+	// 	//LinkedIn link
+	// 	await user.click(screen.getByAltText('The letters i and n representing the LinkedIn logo'));
+	// 	expect(spyAnchorTag).toHaveBeenCalled();
+	// });
 });
