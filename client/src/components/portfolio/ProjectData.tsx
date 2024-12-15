ad7abd7edb4e48449d2f3a766da1119a8a7cf080
imanwarsame <iwarsame38@gmail.com>
2024-12-15 20:16:44 +0000

    Added project

--- client/src/components/portfolio/ProjectData.tsx
+++ client/src/components/portfolio/ProjectData.tsx
@@ -1,6 +1,7 @@
 import WarsameDev from '../../assets/Projects/WarsameDev.png';
 import OpenDetail from '../../assets/Projects/OpenDetail.png';
 import GPI from '../../assets/Projects/GPI.png';
+import Garviz from '../../assets/Projects/Garviz.png';
 
 export const projects = [
 	{
@@ -11,7 +12,14 @@ export const projects = [
 		webLink: 'https://designer.groundplug.dk/',
 	},
 	{
-		id: 2,
+		id: 1,
+		title1: 'Gar',
+		title2: 'viz',
+		imageUrl: Garviz,
+		webLink: 'https://garviz.netlify.app/',
+	},
+	{
+		id: 3,
 		title1: 'Warsame',
 		title2: 'Dev',
 		imageUrl: WarsameDev,
@@ -20,7 +28,7 @@ export const projects = [
 		webLink: 'https://github.com/imanwarsame/warsamedev',
 	},
 	{
-		id: 3,
+		id: 4,
 		title1: 'open',
 		title2: 'detail',
 		imageUrl: OpenDetail,
