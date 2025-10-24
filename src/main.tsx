23f77c396c3e4615f3063572d107e2e96349b483
imanwarsame <iwarsame38@gmail.com>
2025-10-24 22:27:39 +0200

    Remove basename from BrowserRouter and reset Vite base path to root

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 
-const basename = '/warsame-dev/';
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<BrowserRouter basename={basename}>
+		<BrowserRouter>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>,
