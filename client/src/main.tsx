54bb90789f2d558cf0a627fc1bec10b829fa7904
Iman Warsame <iwarsame38@gmail.com>
2025-10-24 21:45:34 +0200

    Set basename for BrowserRouter in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 
+const basename = '/warsame-dev/';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename={basename}>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>,
